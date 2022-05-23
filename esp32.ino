cd403d14be70e5cafa1b68d2a1ff7dc22924094d
Charles Gomes <charlesrg@gmail.com>
2022-05-23 14:32:17 -0400

    Update esp32.ino
    new version now using both cores of the ESP32

--- esp32.ino
+++ esp32.ino
@@ -25,6 +25,11 @@ char buffer[255];
 OneWire oneWire(DS18B20PIN);
 DallasTemperature sensors(&oneWire);
 
+//MULTITASK
+void TaskUpdateSensors( void *pvParameters );
+void TaskUpdateDisplay( void *pvParameters );
+int tempinC;
+
 //ESC controll LIbs
 #include <ESP32Servo.h> // ESP32Servo library installed by Library Manager
 #include "ESC.h" // RC_ESC library installed by Library Manager
@@ -41,8 +46,8 @@ int  MIN_SPEED = PWM_LOW + PWM_LOW / 2; // speed just slow enough to turn motor 
 #define batPin (35)
 int batteryValue = 0;
 //OTA
-const char* ssid = "YOUR_WIFI_SSID";
-const char* password = "WIFI_PASS";
+const char* ssid = "WIFI_SSID";
+const char* password = "password";
 
 //Acelerator Pedal PIN
 const int pedalPin = 34;
@@ -80,8 +85,8 @@ int ledState = LOW;  // ledState used to set the LED
 void setup() {
 
   pinMode(led, OUTPUT);  
-  //Serial.begin(115200);
-  //Serial.println("Booting");
+  Serial.begin(115200);
+  Serial.println("Booting");
 
   EEPROM.begin(EEPROM_SIZE);
   //THROTTLE_FADE = 
@@ -184,65 +189,72 @@ void setup() {
   display.println("Starting Temp Sensor");
   display.display();
   sensors.begin();
+
   display.println("Setup Complete.");
   display.display();
-}
 
+  //multitask
+  Serial.println("Task 1");
+  xTaskCreatePinnedToCore(
+    TaskUpdateSensors
+    ,  "TaskUpdateSensors"   // A name just for humans
+    ,  8192  // This stack size can be checked & adjusted by reading the Stack Highwater
+    ,  NULL
+    ,  1  // Priority, with 3 (configMAX_PRIORITIES - 1) being the highest, and 0 being the lowest.
+    ,  NULL 
+    ,  1);
+  Serial.println("task2");
+  xTaskCreatePinnedToCore(
+    TaskUpdateDisplay
+    ,  "TaskUpdateDisplay"   // A name just for humans
+    ,  8192  // This stack size can be checked & adjusted by reading the Stack Highwater
+    ,  NULL
+    ,  2  // Priority, with 3 (configMAX_PRIORITIES - 1) being the highest, and 0 being the lowest.
+    ,  NULL 
+    ,  1); 
+Serial.println("Tasks sent");
+}
 void loop() {
-  display.clearDisplay();
-  display.setCursor(0,0);
-  if(WiFi.waitForConnectResult() == WL_CONNECTED){
-    ArduinoOTA.handle();  
-  }
   
   // Display static text
+  //Serial.println("Reading PEdal");
   pedalValue = analogRead(pedalPin);
-
-  //Temperature PRotection
-  sensors.requestTemperatures(); 
-  float tempinC = sensors.getTempCByIndex(0);
-  display.print("Temp:");
-  display.println(tempinC);
+  //Serial.println("pedalValue");
+  //Serial.println("Check Temp");
   if (tempinC > 70){
-    if(tempinC > 85){
-      pedalValue=860;
-      display.println("Temperature Shuttoff");
-    }else if(tempinC >80){
+    if(tempinC > 80){
+      pedalValue=860; //display.println("Temperature Shuttoff");
+    }else if(tempinC >75){
       if(pedalValue > 1900) {
-        pedalValue=1200;
+        pedalValue=120+0;
       } 
-      display.println("Temperature Critical");
+      //display.println("Temperature Critical");
     }else {
-      display.println("Temperature Warning!");
+      //display.println("Temperature Warning!");
       if(pedalValue > 1900) {
         pedalValue=1900;
       }
     }
   }
-
-  //Battery voltage Protection  
-  batteryValue=analogRead(batPin);
-  batteryPercentage=map(batteryValue,2000,3320,0,100);
-  sprintf(buffer, "Battery: %d - %d", batteryPercentage, batteryValue);
-  display.println(buffer);
+  //Serial.println("Check Battery");
   if (batteryPercentage < 20){
     if(batteryPercentage < 1){
       pedalValue=860;
-      display.println("Battery too Low");
+      //display.println("Battery too Low");
     }else if(batteryPercentage < 10){
       if(pedalValue > 1900) {
         pedalValue=1200;
       } 
-      display.println("Battery Critical");
+      //display.println("Battery Critical");
     }else {
-      display.println("Battery Warning!");
+      //display.println("Battery Warning!");
       if(pedalValue > 1900) {
         pedalValue=1900;
       }
     }
   }
-
-  
+  //Serial.println("Apply PEdal fade");
+  //pedal value varies between 860 and 1200 at rest
   if (pedalValue < 1200 ){
     pedalValue=860;
   }else if (pedalValue > 2920 ){
@@ -255,22 +267,17 @@ void loop() {
   }else{
       fadedPedalValue=pedalValue;
   }
-  
-  sprintf(buffer, "Ped:%4d Fad:%d", pedalValue, fadedPedalValue);
-  display.println(buffer);
+  //Serial.println("Send to ESC");
+  //sprintf(buffer, "Ped:%4d Fad:%d", pedalValue, fadedPedalValue);
+  //display.println(buffer);
   //display.display();
-  //Switch Position
-  goForward = digitalRead(FORWARD_PIN);
-  goReverse = digitalRead(REVERSE_PIN);
   if ((goForward == HIGH) && (goReverse == LOW)){
     //ESC commands
     pedalValue = map(fadedPedalValue, 860, 2940, 1500, 2000); // scale forward motor
     myESC2.speed(pedalValue); // sets the ESC speed
-    pedalValue = map(fadedPedalValue, 860, 2940, 1500, 1000); // scale inverse motor
     myESC1.speed(pedalValue); // sets the ESC speed
-    display.setTextSize(2);
-    display.println("Forward");
-    display.setTextSize(1);
+    //pedalValue = map(fadedPedalValue, 860, 2940, 1500, 1000); // scale inverse motor
+    //myESC1.speed(pedalValue); // sets the ESC speed
   }
   else if ((goReverse == HIGH) && (goForward == LOW)){
     //ESC commands
@@ -280,31 +287,102 @@ void loop() {
     }
     pedalValue = map(fadedPedalValue, 860, 2940, 1500, 1000); // scale forward motor
     myESC2.speed(pedalValue); // sets the ESC speed
-    pedalValue = map(fadedPedalValue, 860, 2940, 1500, 2000); // scale reverse motor
     myESC1.speed(pedalValue); // sets the ESC speed
-    display.setTextSize(2);
-    display.println("Reverse");
-    display.setTextSize(1);
+    //pedalValue = map(fadedPedalValue, 860, 2940, 1500, 2000); // scale reverse motor
+    //myESC1.speed(pedalValue); // sets the ESC speed
   }
   else {
     myESC1.speed(1500); // sets the ESC speed
     myESC2.speed(1500); // sets the ESC speed
     pedalValue=1500;
-    display.setTextSize(2);
-    display.println("Neutral");
-    display.setTextSize(1);
   }
-  
+  //Serial.println("Check Milis");
   //Programming Mode
   unsigned long currentMillis = millis();
   counter++;
   if (currentMillis - previousMillis >= interval) {
+    //Serial.println("milis");
     previousMillis = millis();
     iterations = counter;
     counter=0;
+  }
+}
+
+void TaskUpdateSensors(void *pvParameters)  { //task
+  (void) pvParameters;
+  
+  for (;;)  {
+    if(WiFi.waitForConnectResult() == WL_CONNECTED){
+      ArduinoOTA.handle();  
+    }
+    sensors.requestTemperatures(); 
+    tempinC = sensors.getTempCByIndex(0);
+    batteryValue=analogRead(batPin);
+    batteryPercentage=map(batteryValue,2000,3320,0,100);
+    //Serial.print("Sensors stack usage: ");
+    //Serial.println(uxTaskGetStackHighWaterMark( NULL ));
+  }
+}
+
+void TaskUpdateDisplay(void *pvParameters)  { //this is a task
+  (void) pvParameters;
+  for (;;) {
+    //Serial.print("programmingMode:");
+    //Serial.println(programmingMode);
+    //if(programmingMode==100) continue; //do not paint the screen while in programming mode.
+    //Switch Position
+    goForward = digitalRead(FORWARD_PIN);
+    goReverse = digitalRead(REVERSE_PIN);
+
+    display.clearDisplay();
+    display.setCursor(0,0);
+    display.setTextSize(2);
+    display.print("T");
+    display.print(tempinC);
+    display.print("C B");
+    display.print(batteryPercentage);
+    display.println("%");
+    //sprintf(buffer, "T:%2dC B:%d%%", tempinC, batteryPercentage);
+    //display.println(buffer);
+    display.setTextSize(1);
+    if (tempinC > 70){
+      if(tempinC > 80){
+        display.println("Temperature Shuttoff");
+      }else if(tempinC >75){
+        display.println("Temperature Critical");
+      }else {
+        display.println("Temperature Warning!");
+      }
+    }else if (tempinC < 10) {
+      display.println("Sensor disconnected ?");
+    }
+    if (batteryPercentage < 20){
+      if(batteryPercentage < 1){
+        display.println("Battery too Low");
+      }else if(batteryPercentage < 10){
+        display.println("Battery Critical");
+      }else {
+        display.println("Battery Warning!");
+      }
+    }
+    display.setTextSize(2);
+    if ((goForward == HIGH) && (goReverse == LOW)){
+      display.println("Forward");
+    }else if ((goReverse == HIGH) && (goForward == LOW)){
+      display.println("Reverse");
+    }else {
+      display.println("Neutral");
+    }
+    display.setTextSize(1);
+    sprintf(buffer, "It:%d Fa:%d,Ba:%d", iterations, THROTTLE_FADE, batteryValue);
+    display.println(buffer);
+    display.display();  
+    //Serial.print("Display stack usage: ");
+    //Serial.println(uxTaskGetStackHighWaterMark( NULL ));
     if(digitalRead(PROGRAMMING_PIN) == LOW){
+      //Serial.println("propinread");
       programmingMode++;
-      if(programmingMode > 5 && goReverse == LOW && goForward == LOW){
+      if(programmingMode > 100 && goReverse == LOW && goForward == LOW){
         iterations=0;
         while(iterations < 20){
           display.clearDisplay();
@@ -312,24 +390,24 @@ void loop() {
           display.println("Programming Mode. Use Button to Switch");
           display.print("Current THROTTLE_FADE:");
           display.println(THROTTLE_FADE);
-          display.println("Options: 100,250,500,1000,2000");
+          display.println("Options: 1,10,50,100,250");
           display.display();
           delay(500);
           if(digitalRead(PROGRAMMING_PIN) == LOW){
-            if(THROTTLE_FADE==100){
+            if(THROTTLE_FADE==1){
+              THROTTLE_FADE=10;
+              iterations=0;
+            }else if(THROTTLE_FADE == 10){
+              THROTTLE_FADE=50;
+              iterations=0;
+            }else if(THROTTLE_FADE == 50){
+              THROTTLE_FADE = 100;
+              iterations=0;
+            }else if(THROTTLE_FADE == 100){
               THROTTLE_FADE=250;
               iterations=0;
-            }else if(THROTTLE_FADE == 250){
-              THROTTLE_FADE=500;
-              iterations=0;
-            }else if(THROTTLE_FADE == 500){
-              THROTTLE_FADE = 1000;
-              iterations=0;
-            }else if(THROTTLE_FADE == 1000){
-              THROTTLE_FADE=2000;
-              iterations=0;
             }else {
-              THROTTLE_FADE=100;
+              THROTTLE_FADE=1;
               iterations=0;
             }
           }else {
@@ -347,11 +425,4 @@ void loop() {
       }
     }
   }
-  
-  sprintf(buffer, "THROTTLE_FADE:%d", THROTTLE_FADE);
-  display.println(buffer);
-
-   
-
-  display.display(); 
 }
